cd88d4a7489181b4360d16c78b0518225552c763
padenhall <paden.hall1@gmail.com>
2025-10-15 20:45:42 -0700

    Add files via upload

---
+++ header_footer.js
@@ -0,0 +1,35 @@
+
+class SpecialHeader extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = 
+        ` 
+        <nav>
+            <ul class="theme-header">
+                <li><a class="specialheading" href ="Orbital Minds.html"> Orbital Minds </a></li>
+                <li><a class="navbar" href ="Work.html"> Work </a></li>
+                <li><a class="navbar" href ="Projects.html"> Projects </a></li>
+                <li><a class="navbar" href ="Research.html"> Research </a></li>
+                <li><a class="navbar" href ="Resume.html"> Resume </a></li>
+                <li><a class="navbar" href ="About.html"> About/Contact </a></li>
+            </ul>  
+        </nav>
+        <link rel="stylesheet "href="Formatting/header_style.css">
+        `
+    }
+}
+
+
+class SpecialFooter extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = 
+        `
+        <h1 class="theme-footer">Information Location</h1>
+        <ul class="theme-footer">
+            <p class="theme-footer">Orbital Minds | Aerospace Manufacturing & Engineering | Pomona, CA | BS Aerospace Engineer| Cal Poly Pomona Alumni </p>
+        </ul>  
+        <link rel="stylesheet "href="Formatting/footer_style.css"> 
+        `
+    }
+}
+customElements.define('special-header', SpecialHeader)
+customElements.define('special-footer', SpecialFooter)
